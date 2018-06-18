0b1a0800b07cda287800349a9e8834ff4ddf0590
Huỳnh Ngọc Hải <moyes.babes@gmail.com>
2018-06-18 10:13:36 +0700

    Repair

--- app/src/main/java/com/framgia/music_18/data/model/Song.java
+++ app/src/main/java/com/framgia/music_18/data/model/Song.java
@@ -1,57 +1,86 @@
 package com.framgia.music_18.data.model;
 
 import android.support.annotation.NonNull;
+import android.widget.ImageView;
 
 public class Song implements Comparable<Song> {
 
-    private long id;
-    private String title;
-    private String artist;
-    private String artworkUrl;
-    private long duration;
-    private String streamUrl;
-    private int playbackCount;
+    private long mId;
+    private String mTitle;
+    private String mArtist;
+    private String mArtworkUrl;
+    private long mDuration;
+    private String mStreamUrl;
+    private int mPlaybackCount;
 
     public Song(long id, String title, String artist, String artworkUrl, long duration, String streamUrl, int playbackCount) {
-        this.id = id;
-        this.title = title;
-        this.artist = artist;
-        this.artworkUrl = artworkUrl;
-        this.duration = duration;
-        this.streamUrl = streamUrl;
-        this.playbackCount = playbackCount;
+        mId = id;
+        mTitle = title;
+        mArtist = artist;
+        mArtworkUrl = artworkUrl;
+        mDuration = duration;
+        mStreamUrl = streamUrl;
+        mPlaybackCount = playbackCount;
     }
 
     public long getId() {
-        return id;
+        return mId;
+    }
+
+    public void setId(long id) {
+        mId = id;
     }
 
     public String getTitle() {
-        return title;
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
     }
 
     public String getArtist() {
-        return artist;
+        return mArtist;
+    }
+
+    public void setArtist(String artist) {
+        mArtist = artist;
     }
 
     public String getArtworkUrl() {
-        return artworkUrl;
+        return mArtworkUrl;
+    }
+
+    public void setArtworkUrl(String artworkUrl) {
+        mArtworkUrl = artworkUrl;
     }
 
     public long getDuration() {
-        return duration;
+        return mDuration;
+    }
+
+    public void setDuration(long duration) {
+        mDuration = duration;
     }
 
     public String getStreamUrl() {
-        return streamUrl;
+        return mStreamUrl;
+    }
+
+    public void setStreamUrl(String streamUrl) {
+        mStreamUrl = streamUrl;
     }
 
     public int getPlaybackCount() {
-        return playbackCount;
+        return mPlaybackCount;
+    }
+
+    public void setPlaybackCount(int playbackCount) {
+        mPlaybackCount = playbackCount;
     }
 
     @Override
     public int compareTo(@NonNull Song another) {
-        return another.playbackCount - this.playbackCount;
+        return another.mPlaybackCount - this.mPlaybackCount;
     }
 }
